8f442716deb4a554712f07171bb6289331ef58a8
keykat <koin12000@naver.com>
2021-08-21 19:00:20 +0900

    2021.8.21 잔디 테스트

--- app/src/main/java/com/keykat/keykat/brawlkat/util/network/Client.java
+++ app/src/main/java/com/keykat/keykat/brawlkat/util/network/Client.java
@@ -59,8 +59,6 @@ public class Client {
     private                 String                          GCPIPADDRESS = "35.237.9.225";
     private                 String                          ORACLEIPADDRESS = "193.122.98.86";
 
-    // 잔디
-
     public Client(){
 
     }
