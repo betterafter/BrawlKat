b54f10b14dc83e1d7145fed22776c2a985333397
keykat <koin12000@naver.com>
2021-09-24 02:05:11 +0900

    [Feat] 설정 - Developer Information 추가
    - 설정 - Developer 추가
    - 개발자 이름, 블로그, 이메일 정보 추가

--- app/src/main/java/com/keykat/keykat/brawlkat/home/setting/activity/DeveloperSummaryActivity.kt
+++ app/src/main/java/com/keykat/keykat/brawlkat/home/setting/activity/DeveloperSummaryActivity.kt
@@ -1,4 +1,18 @@
 package com.keykat.keykat.brawlkat.home.setting.activity
 
-class DeveloperSummaryActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.keykat.keykat.brawlkat.databinding.ActivitySettingsDeveloperSummaryBinding
+
+class DeveloperSummaryActivity : AppCompatActivity() {
+
+    private var _developerSummaryBinding: ActivitySettingsDeveloperSummaryBinding? = null
+    private val binding get() = _developerSummaryBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _developerSummaryBinding = ActivitySettingsDeveloperSummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
