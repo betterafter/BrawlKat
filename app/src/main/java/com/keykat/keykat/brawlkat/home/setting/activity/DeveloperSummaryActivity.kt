1610941594bf30fe492b7678cc67a7ba37cf97e7
keykat <koin12000@naver.com>
2021-09-23 20:58:35 +0900

    [Feat] 설정 - 브롤러 추천 서비스에 대한 간단한 설명 추가
    - 설정 - 서비스 설명에 추가
    - viewpager2로 3장 정도로 간단하게 설명
    - 아예 앱 사용법에 대한 설명으로 바꿀지 고려 중

---
+++ app/src/main/java/com/keykat/keykat/brawlkat/home/setting/activity/DeveloperSummaryActivity.kt
@@ -0,0 +1,4 @@
+package com.keykat.keykat.brawlkat.home.setting.activity
+
+class DeveloperSummaryActivity {
+}
---
+++ app/src/main/java/com/keykat/keykat/brawlkat/home/setting/activity/ServiceSummaryFragment3.kt
@@ -0,0 +1,19 @@
+package com.keykat.keykat.brawlkat.home.setting.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.keykat.keykat.brawlkat.R
+
+class ServiceSummaryFragment3 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.activity_settings_service_summary_fragment_3, container, false
+        ) as ViewGroup
+    }
+}
