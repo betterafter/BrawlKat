f60a74737e489c3d94948ae920271184e62d163c
keykat <koin12000@naver.com>
2021-10-23 03:50:28 +0900

    [HotFix] 이벤트가 업데이트 안 되었던 문제 해결 중

--- app/src/main/java/com/keykat/keykat/brawlkat/service/maprecommendservice/repository/MapRecommendRepositoryImpl.kt
+++ app/src/main/java/com/keykat/keykat/brawlkat/service/maprecommendservice/repository/MapRecommendRepositoryImpl.kt
@@ -1,7 +1,12 @@
 package com.keykat.keykat.brawlkat.service.maprecommendservice.repository
 
 import com.keykat.keykat.brawlkat.service.maprecommendservice.util.MapRecommendDataSource
+import com.keykat.keykat.brawlkat.service.util.NotificationData
 import com.keykat.keykat.brawlkat.util.KatData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MapRecommendRepositoryImpl(
     private val mapRecommendDataSource: MapRecommendDataSource
--- app/src/main/java/com/keykat/keykat/brawlkat/service/maprecommendservice/util/MapRecommendDataSource.kt
+++ app/src/main/java/com/keykat/keykat/brawlkat/service/maprecommendservice/util/MapRecommendDataSource.kt
@@ -1,18 +1,31 @@
 package com.keykat.keykat.brawlkat.service.maprecommendservice.util
 
 import android.content.Context
+import com.keykat.keykat.brawlkat.common.exceptions.InvalidBaseDataException
+import com.keykat.keykat.brawlkat.common.exceptions.InvalidPlayerDataException
+import com.keykat.keykat.brawlkat.service.util.BOUNDARY
+import com.keykat.keykat.brawlkat.service.util.IP
+import com.keykat.keykat.brawlkat.service.util.NotificationData
 import com.keykat.keykat.brawlkat.util.KatData
 import com.keykat.keykat.brawlkat.util.network.BaseApiDataThread
+import com.keykat.keykat.brawlkat.util.parser.kat_brawlersParser
+import com.keykat.keykat.brawlkat.util.parser.kat_eventsParser
+import com.keykat.keykat.brawlkat.util.parser.kat_mapsParser
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketAddress
+import java.nio.charset.StandardCharsets
 
 class MapRecommendDataSource(
     context: Context
 ) {
-    val baseApiDataThread = BaseApiDataThread(context)
+    private val baseApiDataThread = BaseApiDataThread(context)
 
     fun getPlayerInfoData() = KatData.eventsPlayerData.value
+
     fun getMapRecommendViewData(viewCallback: () -> (Unit)) {
-        if(KatData.EventArrayList.size <= 0 || KatData.BrawlersArrayList.size <= 0) {
-            baseApiDataThread.getData(viewCallback)
-        }
+        baseApiDataThread.getData(viewCallback)
     }
 }
